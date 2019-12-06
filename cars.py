510083678687c5b77c2515a68de3db53dddb1c1d
logotip <logotip123@yahoo.com>
2019-12-06 16:25:05 +0200

    v 1.1

--- cars.py
+++ cars.py
@@ -9,6 +9,51 @@ NewCar
 """
 
 
+class Engine:
+    """
+    Representing engine class.
+    Attributes
+    ----------
+    Methods
+    -------
+    get_engine_type_1()
+    get_engine_type_2()
+    """
+    def __init__(self):
+        self._engine_type_1 = "1HZ"
+        self._engine_type_2 = "1HD-T"
+
+    def get_engine_type_1(self):
+        """return engine type 1"""
+        return self._engine_type_1
+
+    def get_engine_type_2(self):
+        """return engine type 2"""
+        return self._engine_type_2
+
+
+class Wheel:
+    """
+    Representing wheel class.
+    Attributes
+    ----------
+    Methods
+    -------
+    get_wheel_type_1()
+    get_wheel_type_2()
+    """
+    def __init__(self):
+        self._wheel_type_1 = "Continental"
+        self._wheel_type_2 = "Pirelli"
+
+    def get_wheel_type_1(self):
+        """return wheel type 1"""
+        return self._wheel_type_1
+
+    def get_wheel_type_2(self):
+        """return wheel type 2"""
+        return self._wheel_type_2
+
 class BaseCar:
     """
     Representing base car class.
@@ -22,6 +67,8 @@ class BaseCar:
     def __init__(self):
         self._engine_type = None
         self._wheel_type = None
+        self._engine = Engine()
+        self._wheel = Wheel()
 
     def get_wheel_type(self):
         """return wheel type"""
@@ -36,21 +83,21 @@ class Car1(BaseCar):
     """Representing car1 class"""
     def __init__(self):
         super().__init__()
-        self._engine_type = "1HZ"
-        self._wheel_type = "Continental"
+        self._engine_type = self._engine.get_engine_type_1()
+        self._wheel_type = self._wheel.get_wheel_type_1()
 
 
 class Car2(BaseCar):
     """Representing car2 class"""
     def __init__(self):
         super().__init__()
-        self._engine_type = "1HD-T"
-        self._wheel_type = "Pirelli"
+        self._engine_type = self._engine.get_engine_type_2()
+        self._wheel_type = self._wheel.get_wheel_type_2()
 
 
 class NewCar(BaseCar):
     """Representing new car class"""
     def __init__(self):
         super().__init__()
-        self._engine_type = Car2().get_engine_type()
-        self._wheel_type = Car1().get_wheel_type()
+        self._engine_type = self._engine.get_engine_type_2()
+        self._wheel_type = self._wheel.get_wheel_type_1()
