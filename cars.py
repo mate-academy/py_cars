275c34f0b1632860fcb4cc0f64f67411d6b59d66
logotip <logotip123@yahoo.com>
2019-12-06 13:47:41 +0200

    v 1.0

--- cars.py
+++ cars.py
@@ -1,10 +1,56 @@
-class Car1:
-    pass
+"""
+Module defines all cars.
+Classes
+-------
+BaseCar
+Car1
+Car2
+NewCar
+"""
 
 
-class Car2:
-    pass
+class BaseCar:
+    """
+    Representing base car class.
+    Attributes
+    ----------
+    Methods
+    -------
+    get_wheel_type()
+    get_engine_type()
+    """
+    def __init__(self):
+        self._engine_type = None
+        self._wheel_type = None
+
+    def get_wheel_type(self):
+        """return wheel type"""
+        return self._wheel_type
+
+    def get_engine_type(self):
+        """return engine type"""
+        return self._engine_type
 
 
-class NewCar:
-    pass
+class Car1(BaseCar):
+    """Representing car1 class"""
+    def __init__(self):
+        super().__init__()
+        self._engine_type = "1HZ"
+        self._wheel_type = "Continental"
+
+
+class Car2(BaseCar):
+    """Representing car2 class"""
+    def __init__(self):
+        super().__init__()
+        self._engine_type = "1HD-T"
+        self._wheel_type = "Pirelli"
+
+
+class NewCar(BaseCar):
+    """Representing new car class"""
+    def __init__(self):
+        super().__init__()
+        self._engine_type = Car2().get_engine_type()
+        self._wheel_type = Car1().get_wheel_type()
