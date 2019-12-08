8491e1c4d5d401d8659117d610e865b8e8f8ccef
Platoniuks <platon@alterpost.org>
2019-12-08 14:39:21 +0200

    resolved

--- cars.py
+++ cars.py
@@ -1,10 +1,31 @@
+"""Design for new cars"""
+
+
 class Car1:
-    pass
+    """Class of car1"""
+    def __init__(self):
+        self.wheel_type = 'Continental'
+        self.engine_type = '1HZ'
+
+    def get_wheel_type(self):
+        """Get wheel type"""
+        return self.wheel_type
+
+    def get_engine_type(self):
+        """Get engine type"""
+        return self.engine_type
 
 
-class Car2:
-    pass
+class Car2(Car1):
+    """Class of car2"""
+    def __init__(self):
+        super().__init__()
+        self.wheel_type = 'Pirelli'
+        self.engine_type = '1HD-T'
 
 
-class NewCar:
-    pass
+class NewCar(Car2):
+    """Class of new car"""
+    def __init__(self):
+        super().__init__()
+        self.wheel_type = 'Continental'
