d440cee70fffb6cf6afb8f5a4fcfbad6d53fa80a
koreyo <KoretsValentyn@gmail.com>
2019-12-08 21:39:24 +0200

    firts

--- cars.py
+++ cars.py
@@ -1,10 +1,46 @@
-class Car1:
-    pass
+"""There are two cars. It is necessary to design a new car
+with wheels from the first, and an engine from the second."""
 
 
-class Car2:
-    pass
+class Car:
+    """Car object. Return wheel and engine type"""
+    def __init__(self):
+        self. wheel_type = None
+        self. engine_type = None
+
+    def get_wheel_type(self):
+        """return wheel type"""
+        return self.wheel_type
+
+    def get_engine_type(self):
+        """return engine type"""
+        return self.engine_type
+
+    def __repr__(self):
+        return f"{self.get_engine_type()},{self.get_wheel_type()}"
 
 
-class NewCar:
-    pass
+class Car1(Car):
+    """First car. Wheel: Continental. Engine: 1HZ"""
+    def __init__(self):
+        super().__init__()
+        self.wheel_type = "Continental"
+        self.engine_type = "1HZ"
+
+
+class Car2(Car):
+    """Second car: Wheel: Pirelli. Engine: 1HD-T"""
+    def __init__(self):
+        super().__init__()
+        self.wheel_type = "Pirelli"
+        self.engine_type = "1HD-T"
+
+
+class NewCar(Car1, Car2):
+    """The new car. Wheels from the firs (Continental),
+    engine from the second (1HD-T)"""
+    def __init__(self):
+        super().__init__()
+        self.wheel_type = Car1().get_wheel_type()
+        self.engine_type = Car2().get_engine_type()
+
--- test_cars.py
+++ test_cars.py
@@ -3,17 +3,20 @@ import cars
 
 def test_car1():
     c = cars.Car1()
+    print(c)
     assert c.get_wheel_type() == 'Continental'
     assert c.get_engine_type() == '1HZ'
 
 
 def test_car2():
     c = cars.Car2()
+    print(c)
     assert c.get_wheel_type() == 'Pirelli'
     assert c.get_engine_type() == '1HD-T'
 
 
 def test_newcar():
     c = cars.NewCar()
+    print(c)
     assert c.get_wheel_type() == 'Continental'
     assert c.get_engine_type() == '1HD-T'
