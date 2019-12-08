5dab4774cd9a8a1f68aaca4f2610fadf00bd3c78
Dima <dima.sapsay@gmail.com>
2019-12-08 09:13:38 +0200

    forth commit

--- cars.py
+++ cars.py
@@ -24,17 +24,21 @@ class MiximCar:
 
 class Car1(MiximCar):
     """Class Car1 has attributes wheel='Continental', engine='1HZ'"""
-    wheel = 'Continental'
-    engine = '1HZ'
+    def __init__(self):
+        self.__class__.wheel = 'Continental'
+        self.__class__.engine = '1HZ'
 
 
 class Car2(MiximCar):
     """Class Car2 has attributes wheel='Pirelli', engine='1HD-T'"""
-    wheel = 'Pirelli'
-    engine = '1HD-T'
+    def __init__(self):
+        self.__class__.wheel = 'Pirelli'
+        self.__class__.engine = '1HD-T'
 
 
 class NewCar(Car1, Car2):
     """Class Newcar has attributes class Car1 wheel_type and class Car2 engine_type"""
-    wheel = Car1.wheel
-    engine = Car2.engine
+    def __init__(self):
+        super().__init__()
+        self.__class__.wheel = Car1.wheel
+        self.__class__.engine = Car2.engine
