bc0b791f4093ae9891fe14710eddd90ce2d450cc
YuPo13 <yuliya.povzlo@gmail.com>
2019-12-09 15:49:28 +0200

    One more trial: I have the features, I re-use them

--- cars.py
+++ cars.py
@@ -4,14 +4,16 @@ and designs prototype of the new car based on these features"""
 
 class Car1:
     """This class represents the car with some tyres and engine"""
+    wheel_type = 'Continental'
+
     def __init__(self):
         """Define the characteristics of Car1 tyres and engine"""
-        self.wheel_type = 'Continental'
         self.engine_type = '1HZ'
 
-    def get_wheel_type(self):
+    @staticmethod
+    def get_wheel_type():
         """Provide Car1 tyres type"""
-        return self.wheel_type
+        return Car1.wheel_type
 
     def get_engine_type(self):
         """Provide Car1 engine type"""
@@ -20,18 +22,20 @@ class Car1:
 
 class Car2:
     """This class represents the car with some tyres and engine too"""
+    engine_type = '1HD-T'
+
     def __init__(self):
         """Define the characteristics of Car2 tyres and engine"""
         self.wheel_type = 'Pirelli'
-        self.engine_type = '1HD-T'
 
     def get_wheel_type(self):
         """Provide Car2 tyres type"""
         return self.wheel_type
 
-    def get_engine_type(self):
+    @staticmethod
+    def get_engine_type():
         """Provide Car2 engine type"""
-        return self.engine_type
+        return Car2.engine_type
 
 
 class NewCar:
@@ -40,8 +44,8 @@ class NewCar:
 
     def __init__(self):
         """Define the characteristics of Car2 tyres and engine"""
-        self.wheel_type = 'Continental'
-        self.engine_type = '1HD-T'
+        self.wheel_type = Car1.get_wheel_type()
+        self.engine_type = Car2.get_engine_type()
 
     def get_wheel_type(self):
         """Provide the Car1 tyres type"""
