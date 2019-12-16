a7e2cf7f06fa9883dd7db5198739da688aaa69bf
Andrii <andrewsotnikov88@gmail.com>
2019-12-16 22:52:51 +0200

    Py_cars solution

--- cars.py
+++ cars.py
@@ -1,10 +1,41 @@
+"""There are two cars. It is necessary to design a new car with wheels
+from the first, and an engine from the second."""
+
+
 class Car1:
-    pass
+    """Class Car1 representation"""
+
+    def __init__(self):
+        self.wheel = 'Continental'
+        self.engine = '1HZ'
+
+    def get_wheel_type(self):
+        """Returns wheel type of Car1"""
+        return self.wheel
+
+    def get_engine_type(self):
+        """Returns engine type of Car1"""
+        return self.engine
 
 
 class Car2:
-    pass
+    """Class Car1 representation"""
+
+    def __init__(self):
+        self.wheel = 'Pirelli'
+        self.engine = '1HD-T'
+
+    def get_wheel_type(self):
+        """Returns wheel type of Car2"""
+        return self.wheel
+
+    def get_engine_type(self):
+        """Returns engine type of Car2"""
+        return self.engine
 
 
-class NewCar:
-    pass
+class NewCar(Car2, Car1):
+    """New Car class representation"""
+    def __init__(self):
+        super().__init__()
+        self.wheel = Car1().wheel
